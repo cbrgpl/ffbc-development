9e4edff5829e166a7c4a10e5ce795f9928c7861a
Dmitry <cybirgpl@gmail.com>
2022-02-12 02:06:19 +0700

    feat(media_overlay_close): добавил кнопку для закрытия media_overlay

---
+++ src/components/composite/TheMediaViewOverlay/partial/CloseIcon.vue
@@ -0,0 +1,15 @@
+<template >
+  <span class="bg-black-lighten bg-opacity-20 w-8 h-8 md:w-6 md:h-6 cursor-pointer rounded-md" >
+    <zIconBase icon="close" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'MediaViewOverlayCloseIcon',
+}
+</script>
+
+<style>
+
+</style>
