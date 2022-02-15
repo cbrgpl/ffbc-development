d71cce6410b51f6b289649b4930a80174cd1a449
Dmitry <cybirgpl@gmail.com>
2022-02-15 21:59:49 +0700

    rework(toast && progress_bar) : оптимизировал добавление новых тостов; зарефакторил progress_bar и оптимизрова его анимирование;

--- src/components/atomic/zProgressBar.vue
+++ src/components/atomic/zProgressBar.vue
@@ -1,67 +1,110 @@
 <template >
   <span
-    class="progress-bar bg-black-lightest" >
+    :ref="setWrapperWidth"
+    class="w-full overflow-hidden bg-black-lightest" >
     <div
+      @transitionend="emitFinish"
       ref="bar"
-      :style="{width: progress + '%'}"
-      class="progress-bar__progress" ></div>
+      :style="barStyles"
+      class="progress-bar__progress w-0 h-full bg-current" ></div>
   </span>
 </template>
 
 <script>
-import { watch, computed, ref } from 'vue'
+import { ErrorWithDescription } from '@errors'
+
 export default {
-  emits: [ 'finish', 'stop' ],
+  emits: [ 'finished' ],
   name: 'zProgressBar',
   props: {
     progress: {
       type: Number,
       required: true,
     },
-    background: {
-      type: String,
-      default: 'transparent',
+    animationPaused: {
+      type: Boolean,
+      default: false,
     },
-    overrun: {
+    durationMs: {
       type: Number,
-      default: 1,
-    },
-    color: {
-      type: String,
-      default: 'current'
+      required: true,
     }
   },
-  setup ( props, context ) {
-    const validateProgress = ( progress ) => progress >= 0 && progress <= max.value
-    const max = computed( () => 100 + props.overrun )
-    const bar = ref( null )
-
-    const unwatch = watch(
-      () => props.progress,
-      ( newValue ) => {
-        if ( !validateProgress( newValue ) ) { console.warn( `Progress with value ${ newValue } in not valid;\n Valid range [0, ${ max.value }]` ) }
-
-        if ( newValue >= 100 ) {
-          context.emit( 'stop' )
-          bar.value.addEventListener( 'transitionend', ( e ) => {
-            context.emit( 'finish' )
-          }, { once: true } )
-          unwatch()
-        }
+  data () {
+    return {
+      barWidth: 0,
+      wrapperWidth: 0,
+      duration: this.durationMs,
+    }
+  },
+  computed: {
+    barStyles () {
+      return {
+        width: this.barWidth + '%',
+        transitionDuration: `${ this.duration }ms`
       }
-    )
+    }
+  },
+  watch: {
+    progress: {
+      handler ( newValue ) {
+        if ( !this.validateProgress( newValue ) ) {
+          throw new ErrorWithDescription( `Invalid progress prop value "${ this.progress }"` )
+        }
 
-    return { bar }
+        setTimeout( () => {
+          this.barWidth = this.progress
+        }, 0 )
+      },
+      immediate: true,
+    },
+    animationPaused ( newValue ) {
+      if ( newValue ) {
+        this.pauseBarAnimation()
+      } else {
+        this.continueBarAnimation()
+      }
+    }
+  },
+  methods: {
+    validateProgress: ( progress ) => progress >= 0 && progress <= 100,
+    pauseBarAnimation () {
+      this.barWidth = this.getBarPercentWidth()
+      this.duration = this.updateDuration( this.barWidth )
+    },
+    getBarPercentWidth () {
+      const barWidth = this.getBarWidth()
+
+      return ( barWidth / this.wrapperWidth ).toFixed( 2 ) * 100
+    },
+    getBarWidth () {
+      const barClientRect = this.$refs.bar.getBoundingClientRect()
+      return barClientRect.width.toFixed( 2 )
+    },
+    updateDuration ( percentWidthOnPause ) {
+      const passedTime = Math.round( this.durationMs * ( percentWidthOnPause / 100 ) )
+      const remainingTime = this.durationMs - passedTime
+      return remainingTime
+    },
+    continueBarAnimation () {
+      this.barWidth = this.progress
+    },
+    setWrapperWidth ( $el ) {
+      if ( $el ) {
+        this.wrapperWidth = $el.getBoundingClientRect().width
+      }
+    },
+    emitFinish () {
+      this.$emit( 'finished' )
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.progress-bar {
-  @apply w-full overflow-hidden;
+.progress-bar__progress {
+  transition-timing-function: linear;
+  transition-property: width;
 }
 
-.progress-bar__progress {
-  @apply w-0 h-full bg-current transition-all duration-200 ease-linear;
-}
 </style>
