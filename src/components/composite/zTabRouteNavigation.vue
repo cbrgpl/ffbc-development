1190bd6509eb1ffb46d3a30a83fb4c227ab6e8f2
Dmitry <cybirgpl@gmail.com>
2022-02-21 01:04:25 +0700

    rework(tab_nav): прокинул пропсы в слот для tabNav; Написал обертку для использования router-link вместе с tabNav

---
+++ src/components/composite/zTabRouteNavigation.vue
@@ -0,0 +1,33 @@
+<template >
+  <zTabNav v-model="modelValue" >
+    <template v-slot="{tab}" >
+      <slot :tab="tab" ></slot>
+    </template>
+  </zTabNav>
+</template>
+
+<script>
+import zTabNav from '@/components/atomic/zTabNav.vue'
+
+export default {
+  name: 'zTabRouteNavigation',
+  props: {
+    initialValue: {
+      type: Object,
+      required: true,
+    }
+  },
+  data () {
+    return {
+      modelValue: this.initialValue,
+    }
+  },
+  components: {
+    zTabNav
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
