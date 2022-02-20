43b77d0dc00bed2c3196d77dafdcf3c7d961a64b
Dmitry <cybirgpl@gmail.com>
2022-02-21 01:12:03 +0700

    feat(user-layout): сделал user-layout; сделал переход к одной из страниц user-layout; создал enum-навигацию для User

---
+++ src/enums/nav/userNavigation.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'Me',
+    toRouteName: 'UserMe',
+    icon: 'manageAccount'
+  },
+  {
+    name: 'Account',
+    toRouteName: 'UserAccount',
+    icon: 'code'
+  }
+]
