088d4264ea4dbddf4a1fc3d3fd8723ba21f38faf
Dmitry <cybirgpl@gmail.com>
2022-03-30 20:33:42 +0700

    feat(order_refinements): доработал мерки на странице order_details; добавил тост при оформлении заказа; добавил прелоадер к странице order_details;

---
+++ src/enums/backend/orderStatusTypes.js
@@ -0,0 +1,18 @@
+export default {
+  AWAITING_PAYMENT: {
+    id: 1,
+    value: 'Awaiting Payment'
+  },
+  IN_THE_WORK: {
+    id: 2,
+    value: 'In the work'
+  },
+  IS_MADE: {
+    id: 3,
+    value: 'Is made'
+  },
+  SENT: {
+    id: 4,
+    value: 'Sent'
+  },
+}
---
+++ src/views/Shop/ProfileOrderDetail/partial/OrderDetailsMeasuresSection.vue
@@ -0,0 +1,44 @@
+<template >
+  <section class="mb-5 pb-2" >
+    <h4 class="underline mb-2.5" >
+      {{ name }}
+    </h4>
+
+    <ul >
+      <li
+        class="mb-1"
+        v-for="measureFieldWithName of measureFieldsWithName"
+        :key="measureFieldWithName.measureField.measureField" >
+        <b >{{ measureFieldWithName.name }}:&nbsp;</b>
+        <i >{{ measureFieldWithName.measureField.value }}</i>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'OrderDetailsMeasuresSection',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    measureFields: {
+      type: Array,
+      required: true,
+    }
+  },
+  computed: {
+    measureFieldsWithName () {
+      const getMeasureFieldWithName = this.$store.getters[ 'measure/measureFieldWithName' ]
+      const getMeasureFieldId = ( measureField ) => measureField.measureField
+      return this.measureFields.map( ( measureField ) => getMeasureFieldWithName( measureField, getMeasureFieldId ) )
+    }
+  }
+}
+</script>
+
+<style lang="">
+
+</style>
