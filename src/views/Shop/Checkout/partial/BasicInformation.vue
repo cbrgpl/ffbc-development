4ae1809b6abe02e07b6e1d5209ba3515951f7441
Dmitry <cybirgpl@gmail.com>
2022-04-24 21:17:24 +0700

    feat(checkout_section): made section actions and basic_information template;

---
+++ src/views/Shop/Checkout/partial/BasicInformation.vue
@@ -0,0 +1,34 @@
+<template >
+  <div >
+    <h3 >BasicInformation</h3>
+    <div >
+
+    </div>
+
+    <SectionActions @show-next="emitSectionComplete" />
+  </div>
+</template>
+
+<script>
+import SectionActions from './SectionActions.vue'
+
+import formValidations from './../validations/basicInformation'
+
+export default {
+  name: 'BasicInformation',
+  emits: [ 'section-complete' ],
+  methods: {
+    emitSectionComplete () {
+      this.$emit( 'section-complete' )
+    }
+  },
+  components: {
+    SectionActions
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
