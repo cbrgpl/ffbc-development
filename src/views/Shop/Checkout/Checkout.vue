33d6f2b8dc8ee59ec59ac2c34b6c7d61f978403f
Dmitry <cybirgpl@gmail.com>
2022-04-24 21:17:24 +0700

    feat(checkout_section): made section actions and basic_information template;

--- src/views/Shop/Checkout/Checkout.vue
+++ src/views/Shop/Checkout/Checkout.vue
@@ -1,34 +1,66 @@
 <template >
-  <section class="px-3">
-    <Navigation class="mt-2 mb-4 max-w-md md:max-w-lg mx-auto" v-model="currentSectionIndex" :checkoutNavigation="checkoutNavigation"/>
+  <section class="px-3" >
+    <Navigation
+      class="mt-2 mb-4 max-w-md md:max-w-lg mx-auto"
+      v-model="currentSectionIndex"
+      :checkout-navigation="checkoutNavigation" />
 
-    <zButton @click="changeSection">change</zButton>
-    {{ currentSectionIndex }}
+    <component
+      @section-complete="sendSectionData"
+      :is="sectionName" >
+
+      <template #actions >
+        <zButton >
+          change
+        </zButton>
+      </template>
+    </component>
+
   </section>
 </template>
 
 <script>
-import Navigation from './partial/Navigation'
-
+import { computed } from 'vue'
 import checkoutNavigation from '@enums/info/navigation.checkout.js'
+
+import Navigation from './partial/Navigation.vue'
+
+import BasicInformation from './partial/BasicInformation.vue'
 
 export default {
   name: 'Checkout',
-  data() {
+  data () {
     return {
       checkoutNavigation,
-        currentSectionIndex: 0,
-      }
+      currentSectionIndex: 0,
+    }
+  },
+  provide () {
+    return {
+      actionsDisabled: computed( () => this.currentSectionIndex === this.checkoutNavigation.tabs.length - 1 )
+    }
+  },
+  computed: {
+    sectionKeyword () {
+      return this.checkoutNavigation.tabs[ this.currentSectionIndex ].keyword
+    },
+    sectionName () {
+      return this.sectionKeyword.slice( 0, 1 ).toUpperCase() + this.sectionKeyword.slice( 1 )
+    }
   },
   methods: {
-changeSection() {
-  if(this.currentSectionIndex < this.checkoutNavigation.tabs.length - 1) {
-    this.currentSectionIndex++
-  }
-}
+    sendSectionData () {
+      this.changeSection()
+    },
+    changeSection () {
+      if ( this.currentSectionIndex < this.checkoutNavigation.tabs.length - 1 ) {
+        this.currentSectionIndex++
+      }
+    }
   },
   components: {
-    Navigation
+    Navigation,
+    BasicInformation
   }
 }
 </script>
---
+++ src/views/Shop/Checkout/partial/SectionActions.vue
@@ -0,0 +1,28 @@
+<template >
+  <div >
+    {{ actionsDisabled.value }}
+    <zButton @click="showNext" >
+      Next
+    </zButton>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SectionActions',
+  emits: [ 'show-next' ],
+  inject: [
+    'actionsDisabled'
+  ],
+  methods: {
+    showNext () {
+      this.$emit( 'show-next' )
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
