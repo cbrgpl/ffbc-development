ece710fbd72bcb52664a1a0a9c961fb6d94d50b6
Dmitry <cybirgpl@gmail.com>
2022-06-23 19:44:02 +0700

    rework(router && app): removed strategy directory and strategy class; removed removeTemplateLoader storage event emiting on verification page; cleared App inners about template_loader;

--- src/helpers/strategy/base_strategies/strategy.js
+++
@@ -1,7 +0,0 @@
-import { ErrorWithDescription } from '@errors'
-
-export default class Strategy {
-  async execute () {
-    throw new ErrorWithDescription( 'strategy execute method is not implemented' )
-  }
-}
