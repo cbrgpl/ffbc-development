04f56b02733fd63427f168d6f64032b71bfa26b1
cbrgpl <cbrgpl@gmail.com>
2023-05-06 14:15:37 +0700

    fdf

---
+++ src/enums/consts.js
@@ -0,0 +1,26 @@
+const BACKEND_ADDRESS = 'rainbow-developers.site'
+
+const API_URL = `https://${ BACKEND_ADDRESS }/api`
+
+const CLIENT_URL = location.origin
+
+const STATUS_WORDS = {
+  SUCCESS: 'SUCCESS',
+  NOT_ERROR: 'NOT_ERROR',
+  ERROR: 'ERROR',
+}
+const NOT_ERROR = 'NOT_ERROR'
+
+const STORAGE_NAMES = {
+  EMAIL_VERIFICATED: 'flag_emailVerified',
+  REGISTRATED_EMAIL: 'var_regEmail',
+  REFRESH_TOKEN: 'var_refreshToken'
+}
+
+export {
+  NOT_ERROR,
+  API_URL,
+  CLIENT_URL,
+  STORAGE_NAMES,
+  STATUS_WORDS
+}
